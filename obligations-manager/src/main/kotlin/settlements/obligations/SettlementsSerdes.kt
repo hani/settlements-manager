8f7ecb0a2d19e05ddd600932a9876a72d1c21925
Hani Suleiman <hani.suleiman@accenture.com>
2018-03-21 16:38:46 -0400

    Config can be a field

--- obligations-manager/src/main/kotlin/settlements/obligations/SettlementsSerdes.kt
+++ obligations-manager/src/main/kotlin/settlements/obligations/SettlementsSerdes.kt
@@ -12,22 +12,21 @@ import settlements.ObligationState
  * @author Hani Suleiman
  */
 object SettlementsSerdes {
-  
+
+  private val config = mapOf(
+      "specific.avro.reader" to true,
+      "schema.registry.url" to "http://ignored"
+  )
+
   fun obligation(client: SchemaRegistryClient): Serde<Obligation> {
     val deserializer = KafkaAvroDeserializer(client)
-    deserializer.configure(mapOf(
-        "specific.avro.reader" to true,
-        "schema.registry.url" to "http://dummy"
-    ), false)
+    deserializer.configure(config, false)
     return Serdes.serdeFrom<Any>(KafkaAvroSerializer(client), deserializer) as Serde<Obligation>
   }
   
   fun obligationState(client: SchemaRegistryClient): Serde<ObligationState> {
     val deserializer = KafkaAvroDeserializer(client)
-    deserializer.configure(mapOf(
-        "specific.avro.reader" to true,
-        "schema.registry.url" to "http://dummy"
-    ), false)
+    deserializer.configure(config, false)
     return Serdes.serdeFrom<Any>(KafkaAvroSerializer(client), deserializer) as Serde<ObligationState>
   }
 }
